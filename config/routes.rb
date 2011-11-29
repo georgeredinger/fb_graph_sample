b4a5485be91a473a934af972b53829ac8104d546
George Redinger <george@georgeredinger.com>
2011-11-29 11:59:38 -0800

    rails 3.1 routing change?

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ FbGraphSample::Application.routes.draw do
   resource :facebook, :except => :create do
     get :callback, :to => :create
   end
-  resource :dashboard, :only => :show
+  resource :dashboard, :only => :show,:controller=>"dashboard"
   resource :profile, :only => :show
   resource :timeline, :only => [:show, :create]
   resources :subscriptions, :only => [:index, :show, :create]
